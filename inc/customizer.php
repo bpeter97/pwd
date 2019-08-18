b2dd6de8950f8b2d8664f576dd89c58580f450a8
Brian Peter Jr MAC <brian.lee.peter@gmail.com>
2019-08-18 14:09:09 -0700

    Create Contact page and add options to customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -13,146 +13,150 @@
 *               1.2.1.1 about_page_team_brian_name
 *               1.2.1.2 about_page_team_brian_title
 *               1.2.1.3 about_page_team_brian_image
+*               1.2.1.4 about_page_team_brian_image_alt
 *         1.2.2 Chelsea's Section
 *               1.2.2.1 about_page_team_chelsea_name
 *               1.2.2.2 about_page_team_chelsea_title
 *               1.2.2.3 about_page_team_chelsea_image
+*               1.2.2.4 about_page_team_chelsea_image_alt
 *   2. Contact Form
 *     2.1 contact_form_shortcode
 *     2.2 contact_form_message
 *   3. Mission Statement
 *     3.1 mission_statement_message
+*   4. Contact Page
+*     4.1 contact_page_header_text
 */
 
 function pdw_customize_register( $wp_customize ) {
 
   // ********** 1. About Page Page **********
-  $wp_customize->add_panel('about_page', array(
-    'priority'    => 111,
-    'title'       => __('About Page', 'peterwebdev'),
-    'description' => __('Edit the options for the about page', 'peterwebdev')
-  ));
+    $wp_customize->add_panel('about_page', array(
+      'priority'    => 111,
+      'title'       => __('About Page', 'peterwebdev'),
+      'description' => __('Edit the options for the about page', 'peterwebdev')
+    ));
 
-  // ********** 1.1 About Page Header Section **********
-  $wp_customize->add_section('about_page_header', array(
-    'title'       => __('Header Options', 'peterwebdev'),
-    'panel'       => 'about_page',
-    'description' => __('Options for the Header Section', 'peterwebdev'),
-    'priority'    => 1
-  ));
+    // ********** 1.1 About Page Header Section **********
+    $wp_customize->add_section('about_page_header', array(
+      'title'       => __('Header Options', 'peterwebdev'),
+      'panel'       => 'about_page',
+      'description' => __('Options for the Header Section', 'peterwebdev'),
+      'priority'    => 1
+    ));
 
-  // ********** 1.1.1 About Page Header Image **********
-  $wp_customize->add_setting('about_page_header_image', array(
-    'default' => get_bloginfo('template_directory').'/assets/imgs/headers/about-bg.jpg',
-    'type'    => 'theme_mod'
-  ));
-  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'about_page_header_image', array(
-    'label'    => __('Header Image', 'peterwebdev'),
-    'section'  => 'about_page_header',
-    'settings' => 'about_page_header_image',
-    'priority' => 1
-  )));
+    // ********** 1.1.1 About Page Header Image **********
+    $wp_customize->add_setting('about_page_header_image', array(
+      'default' => get_bloginfo('template_directory').'/assets/imgs/headers/about-bg.jpg',
+      'type'    => 'theme_mod'
+    ));
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'about_page_header_image', array(
+      'label'    => __('Header Image', 'peterwebdev'),
+      'section'  => 'about_page_header',
+      'settings' => 'about_page_header_image',
+      'priority' => 1
+    )));
 
-  // ********** 1.1.2 About Page Header Title **********
-  $wp_customize->add_setting('about_page_header_title', array(
-    'default' => 'About',
-    'type'    => 'theme_mod'
-  ));
-  $wp_customize->add_control('about_page_header_title', array(
-    'type'     => 'text',
-    'label'    => __('Header Title', 'peterwebdev'),
-    'section'  => 'about_page_header',
-    'settings' => 'about_page_header_title',
-    'priority' => 2
-  ));
+    // ********** 1.1.2 About Page Header Title **********
+    $wp_customize->add_setting('about_page_header_title', array(
+      'default' => 'About',
+      'type'    => 'theme_mod'
+    ));
+    $wp_customize->add_control('about_page_header_title', array(
+      'type'     => 'text',
+      'label'    => __('Header Title', 'peterwebdev'),
+      'section'  => 'about_page_header',
+      'settings' => 'about_page_header_title',
+      'priority' => 2
+    ));
 
-  // ********** 1.1.3 About Page Header Text **********
-  $wp_customize->add_setting('about_page_header_text', array(
-    'default' => 'Header Text: Edit this in the customizer.',
-    'type'    => 'theme_mod'
-  ));
-  $wp_customize->add_control('about_page_header_text', array(
-    'type'     => 'textarea',
-    'label'    => __('Header Text', 'peterwebdev'),
-    'section'  => 'about_page_header',
-    'settings' => 'about_page_header_text',
-    'priority' => 3
-  ));
+    // ********** 1.1.3 About Page Header Text **********
+    $wp_customize->add_setting('about_page_header_text', array(
+      'default' => 'Header Text: Edit this in the customizer.',
+      'type'    => 'theme_mod'
+    ));
+    $wp_customize->add_control('about_page_header_text', array(
+      'type'     => 'textarea',
+      'label'    => __('Header Text', 'peterwebdev'),
+      'section'  => 'about_page_header',
+      'settings' => 'about_page_header_text',
+      'priority' => 3
+    ));
 
-  // ********** 1.1.4 About Page Video URL **********
-  $wp_customize->add_setting('about_page_youtube_id', array(
-    'default' => 'Header Text: Edit this in the customizer.',
-    'type'    => 'theme_mod'
-  ));
-  $wp_customize->add_control('about_page_youtube_id', array(
-    'type'     => 'text',
-    'label'    => __('Youtube Video ID', 'peterwebdev'),
-    'section'  => 'about_page_header',
-    'settings' => 'about_page_youtube_id',
-    'priority' => 4
-  ));
+    // ********** 1.1.4 About Page Video URL **********
+    $wp_customize->add_setting('about_page_youtube_id', array(
+      'default' => 'Header Text: Edit this in the customizer.',
+      'type'    => 'theme_mod'
+    ));
+    $wp_customize->add_control('about_page_youtube_id', array(
+      'type'     => 'text',
+      'label'    => __('Youtube Video ID', 'peterwebdev'),
+      'section'  => 'about_page_header',
+      'settings' => 'about_page_youtube_id',
+      'priority' => 4
+    ));
 
-  // ********** 1.2 Team Members **********
-  $wp_customize->add_section('about_page_team', array(
-    'title'       => __('Team Member Options', 'peterwebdev'),
-    'panel'       => 'about_page',
-    'description' => __('Options for the Team Members Section', 'peterwebdev'),
-    'priority'    => 2
-  ));
+    // ********** 1.2 Team Members **********
+    $wp_customize->add_section('about_page_team', array(
+      'title'       => __('Team Member Options', 'peterwebdev'),
+      'panel'       => 'about_page',
+      'description' => __('Options for the Team Members Section', 'peterwebdev'),
+      'priority'    => 2
+    ));
 
-  // ********** 1.2.1 Brian's Section **********
-  // ********** 1.2.1.1 Brian's Name **********
-  $wp_customize->add_setting('about_page_team_brian_name', array(
-    'default' => 'Brian L. Peter Jr.',
-    'type'    => 'theme_mod'
-  ));
-  $wp_customize->add_control('about_page_team_brian_name', array(
-    'type'     => 'text',
-    'label'    => __('Brian\'s Name', 'peterwebdev'),
-    'section'  => 'about_page_team',
-    'settings' => 'about_page_team_brian_name',
-    'priority' => 1
-  ));
+    // ********** 1.2.1 Brian's Section **********
+      // ********** 1.2.1.1 Brian's Name **********
+      $wp_customize->add_setting('about_page_team_brian_name', array(
+        'default' => 'Brian L. Peter Jr.',
+        'type'    => 'theme_mod'
+      ));
+      $wp_customize->add_control('about_page_team_brian_name', array(
+        'type'     => 'text',
+        'label'    => __('Brian\'s Name', 'peterwebdev'),
+        'section'  => 'about_page_team',
+        'settings' => 'about_page_team_brian_name',
+        'priority' => 1
+      ));
 
-  // ********** 1.2.1.2 Brian's Title **********
-  $wp_customize->add_setting('about_page_team_brian_title', array(
-    'default' => 'Owner / Web Developer',
-    'type'    => 'theme_mod'
-  ));
-  $wp_customize->add_control('about_page_team_brian_title', array(
-    'type'     => 'text',
-    'label'    => __('Brian\'s Title', 'peterwebdev'),
-    'section'  => 'about_page_team',
-    'settings' => 'about_page_team_brian_title',
-    'priority' => 2
-  ));
+      // ********** 1.2.1.2 Brian's Title **********
+      $wp_customize->add_setting('about_page_team_brian_title', array(
+        'default' => 'Owner / Web Developer',
+        'type'    => 'theme_mod'
+      ));
+      $wp_customize->add_control('about_page_team_brian_title', array(
+        'type'     => 'text',
+        'label'    => __('Brian\'s Title', 'peterwebdev'),
+        'section'  => 'about_page_team',
+        'settings' => 'about_page_team_brian_title',
+        'priority' => 2
+      ));
 
-  // ********** 1.2.1.3 Brian's Image **********
-  $wp_customize->add_setting('about_page_team_brian_image', array(
-    'default' => get_bloginfo('template_directory').'/assets/imgs/team/brian.png',
-    'type'    => 'theme_mod'
-  ));
-  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'about_page_team_brian_image', array(
-    'label'    => __('Brian\'s Image', 'peterwebdev'),
-    'section'  => 'about_page_team',
-    'settings' => 'about_page_team_brian_image',
-    'priority' => 3
-  )));
+      // ********** 1.2.1.3 Brian's Image **********
+      $wp_customize->add_setting('about_page_team_brian_image', array(
+        'default' => get_bloginfo('template_directory').'/assets/imgs/team/brian.png',
+        'type'    => 'theme_mod'
+      ));
+      $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'about_page_team_brian_image', array(
+        'label'    => __('Brian\'s Image', 'peterwebdev'),
+        'section'  => 'about_page_team',
+        'settings' => 'about_page_team_brian_image',
+        'priority' => 3
+      )));
 
-  // ********** 1.2.1.4 Brian's Image Alt **********
-  $wp_customize->add_setting('about_page_team_brian_image_alt', array(
-    'default' => 'An image of Brian L. Peter Jr.',
-    'type'    => 'theme_mod'
-  ));
-  $wp_customize->add_control('about_page_team_brian_image_alt', array(
-    'type'     => 'text',
-    'label'    => __('Brian\'s Image Alt', 'peterwebdev'),
-    'section'  => 'about_page_team',
-    'settings' => 'about_page_team_brian_image_alt',
-    'priority' => 4
-  ));
+      // ********** 1.2.1.4 Brian's Image Alt **********
+      $wp_customize->add_setting('about_page_team_brian_image_alt', array(
+        'default' => 'An image of Brian L. Peter Jr.',
+        'type'    => 'theme_mod'
+      ));
+      $wp_customize->add_control('about_page_team_brian_image_alt', array(
+        'type'     => 'text',
+        'label'    => __('Brian\'s Image Alt', 'peterwebdev'),
+        'section'  => 'about_page_team',
+        'settings' => 'about_page_team_brian_image_alt',
+        'priority' => 4
+      ));
 
-  // ********** 1.2.2 Chelsea's Section **********
+    // ********** 1.2.2 Chelsea's Section **********
     // ********** 1.2.2.1 Chelsea's Name **********
     $wp_customize->add_setting('about_page_team_chelsea_name', array(
       'default' => 'Chelsea C. Peter',
@@ -207,39 +211,41 @@ function pdw_customize_register( $wp_customize ) {
   // ************************************************************************
 
   // ********** 2. Contact Form **********
-  $wp_customize->add_section('contact_form', array(
-    'title'       => __('Contact Form', 'peterwebdev'),
-    'description' => __('Options for the Contact Form Section', 'peterwebdev'),
-    'priority'    => 113
-  ));
+    $wp_customize->add_section('contact_form', array(
+      'title'       => __('Contact Form', 'peterwebdev'),
+      'description' => __('Options for the Contact Form Section', 'peterwebdev'),
+      'priority'    => 113
+    ));
 
-  // ********** 2.1 Contact Form Shortcode **********
-  $wp_customize->add_setting('contact_form_shortcode', array(
-    'default' => 'Contact Form Shortcode: Edit this in the customizer.',
-    'type'    => 'theme_mod'
-  ));
-  $wp_customize->add_control('contact_form_shortcode', array(
-    'type'     => 'text',
-    'label'    => __('Contact Form Shortcode', 'peterwebdev'),
-    'section'  => 'contact_form',
-    'settings' => 'contact_form_shortcode',
-    'priority' => 1
-  ));
+    // ********** 2.1 Contact Form Shortcode **********
+      $wp_customize->add_setting('contact_form_shortcode', array(
+        'default' => 'Contact Form Shortcode: Edit this in the customizer.',
+        'type'    => 'theme_mod'
+      ));
+      $wp_customize->add_control('contact_form_shortcode', array(
+        'type'     => 'text',
+        'label'    => __('Contact Form Shortcode', 'peterwebdev'),
+        'section'  => 'contact_form',
+        'settings' => 'contact_form_shortcode',
+        'priority' => 1
+      ));
 
-  // 2.2 Contact Form Message 
-  $wp_customize->add_setting('contact_form_message', array(
-    'default' => 'Contact Form Message: Edit this in the customizer.',
-    'type'    => 'theme_mod'
-  ));
-  $wp_customize->add_control('contact_form_message', array(
-    'type'     => 'textarea',
-    'label'    => __('Contact Form Message', 'peterwebdev'),
-    'section'  => 'contact_form',
-    'settings' => 'contact_form_message',
-    'priority' => 2
-  ));
+    // ********** 2.2 Contact Form Message **********
+      $wp_customize->add_setting('contact_form_message', array(
+        'default' => 'Contact Form Message: Edit this in the customizer.',
+        'type'    => 'theme_mod'
+      ));
+      $wp_customize->add_control('contact_form_message', array(
+        'type'     => 'textarea',
+        'label'    => __('Contact Form Message', 'peterwebdev'),
+        'section'  => 'contact_form',
+        'settings' => 'contact_form_message',
+        'priority' => 2
+      ));
 
-    // ********** 3. Mission Statement **********
+  // ************************************************************************
+
+  // ********** 3. Mission Statement **********
     $wp_customize->add_section('mission_statement', array(
       'title'       => __('Mission Statement', 'peterwebdev'),
       'description' => __('Options for the Mission Statement Section', 'peterwebdev'),
@@ -247,15 +253,37 @@ function pdw_customize_register( $wp_customize ) {
     ));
   
     // ********** 3.1 Mission Statement Message **********
-    $wp_customize->add_setting('mission_statement_message', array(
-      'default' => 'Mission Statement Message: Edit this in the customizer.',
+      $wp_customize->add_setting('mission_statement_message', array(
+        'default' => 'Mission Statement Message: Edit this in the customizer.',
+        'type'    => 'theme_mod'
+      ));
+      $wp_customize->add_control('mission_statement_message', array(
+        'type'     => 'text',
+        'label'    => __('Mission Statement Message', 'peterwebdev'),
+        'section'  => 'mission_statement',
+        'settings' => 'mission_statement_message',
+        'priority' => 1
+      ));
+
+  // ************************************************************************
+
+  // ********** 4. Contact Page **********
+  $wp_customize->add_section('contact_page', array(
+    'title'       => __('Contact Page', 'peterwebdev'),
+    'description' => __('Options for the Contact Pace', 'peterwebdev'),
+    'priority'    => 114
+  ));
+
+  // ********** 4.1 Contact Page Message **********
+    $wp_customize->add_setting('contact_page_message', array(
+      'default' => 'Contact Page Message: Edit this in the customizer.',
       'type'    => 'theme_mod'
     ));
-    $wp_customize->add_control('mission_statement_message', array(
-      'type'     => 'text',
-      'label'    => __('Mission Statement Message', 'peterwebdev'),
-      'section'  => 'mission_statement',
-      'settings' => 'mission_statement_message',
+    $wp_customize->add_control('contact_page_message', array(
+      'type'     => 'textarea',
+      'label'    => __('Contact Page Message', 'peterwebdev'),
+      'section'  => 'contact_page',
+      'settings' => 'contact_page_message',
       'priority' => 1
     ));
 }
