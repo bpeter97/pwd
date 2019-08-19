d0083b5e12beb41b8adb8b6f1117042c076c245f
bpeter97 <brian.lee.peter@gmail.com>
2019-08-19 13:45:56 -0700

    Add server features

--- template-parts/servers.php
+++ template-parts/servers.php
@@ -1,141 +1,136 @@
 <section id="servers">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 text-center">
-          <h2 class="text-muted text-uppercase pb-5">
-            Shared Web hosting
-          </h2>
-        </div>
-      </div>
-      <div class="row text-center">
-        <div class="col-12 col-md-4 text-center">
-          <div class="card" style="">
-            <div class="card-header">
-              <h2 class="card-title">Basic</h2>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Unlimited</strong> Bandwidth</li>
-              <li class="list-group-item"><strong>Unlimited</strong> Disk Space</li>
-              <li class="list-group-item">1 Domain</li>
-              <li class="list-group-item">
-                <h4 class="card-title">$12.99 / Month</h4>
-                <h5 class="card-title">$150.00 / Year</h5>
-              </li>
-            </ul>
-            <div class="card-footer">
-              <button class="btn btn-primary">Learn More</button>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-4 text-center">
-          <div class="card" style="">
-            <div class="card-header">
-              <h2 class="card-title">Advanced</h2>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Unlimited</strong> Bandwidth</li>
-              <li class="list-group-item"><strong>Unlimited</strong> Disk Space</li>
-              <li class="list-group-item">4 Domains</li>
-              <li class="list-group-item">
-                <h4 class="card-title">$15.99 / Month</h4>
-                <h5 class="card-title">$180.00 / Year</h5>
-              </li>
-            </ul>
-            <div class="card-footer">
-              <button class="btn btn-primary">Learn More</button>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-4 text-center">
-          <div class="card" style="">
-            <div class="card-header">
-              <h2 class="card-title">Ultimate</h2>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Unlimited</strong> Bandwidth</li>
-              <li class="list-group-item"><strong>Unlimited</strong> Disk Space</li>
-              <li class="list-group-item"><strong>Unlimited</strong> Domains</li>
-              <li class="list-group-item">
-                <h4 class="card-title">$19.99 / Month</h4>
-                <h5 class="card-title">$220.00 / Year</h5>
-              </li>
-            </ul>
-            <div class="card-footer">
-              <button class="btn btn-primary">Learn More</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+<div class="container pb-5 pt-5">
+  <div class="row text-center">
 
-<section id="businessServers">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 text-center">
-          <h2 class="text-muted text-uppercase pb-5">
-            Business Web hosting
-          </h2>
+    <div class="col-12 col-md-4 text-center">
+      <div class="card">
+        <div class="card-header">
+          <h2 class="card-title">Shared Hosting</h2>
         </div>
-      </div>
-      <div class="row text-center">
-        <div class="col-12 col-md-4 text-center">
-          <div class="card" style="">
-            <div class="card-header">
-              <h2 class="card-title">Basic</h2>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Unlimited</strong> Bandwidth</li>
-              <li class="list-group-item"><strong>Unlimited</strong> Disk Space</li>
-              <li class="list-group-item">1 Domain</li>
-              <li class="list-group-item">
-                <h4 class="card-title">$12.99 / Month</h4>
-                <h5 class="card-title">$150.00 / Year</h5>
-              </li>
-            </ul>
-            <div class="card-footer">
-              <button class="btn btn-primary">Learn More</button>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-4 text-center">
-          <div class="card" style="">
-            <div class="card-header">
-              <h2 class="card-title">Advanced</h2>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Unlimited</strong> Bandwidth</li>
-              <li class="list-group-item"><strong>Unlimited</strong> Disk Space</li>
-              <li class="list-group-item">4 Domains</li>
-              <li class="list-group-item">
-                <h4 class="card-title">$15.99 / Month</h4>
-                <h5 class="card-title">$180.00 / Year</h5>
-              </li>
-            </ul>
-            <div class="card-footer">
-              <button class="btn btn-primary">Learn More</button>
-            </div>
-          </div>
-        </div>
-        <div class="col-12 col-md-4 text-center">
-          <div class="card" style="">
-            <div class="card-header">
-              <h2 class="card-title">Ultimate</h2>
-            </div>
-            <ul class="list-group list-group-flush">
-              <li class="list-group-item"><strong>Unlimited</strong> Bandwidth</li>
-              <li class="list-group-item"><strong>Unlimited</strong> Disk Space</li>
-              <li class="list-group-item"><strong>Unlimited</strong> Domains</li>
-              <li class="list-group-item">
-                <h4 class="card-title">$19.99 / Month</h4>
-                <h5 class="card-title">$220.00 / Year</h5>
-              </li>
-            </ul>
-            <div class="card-footer">
-              <button class="btn btn-primary">Learn More</button>
-            </div>
-          </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><strong>Unlimited</strong> Bandwidth</li>
+          <li class="list-group-item"><strong>Unlimited</strong> Disk Space</li>
+          <li class="list-group-item">1 to Unlimited Domains</li>
+          <li class="list-group-item">
+            <p class="my-1">Packages starting at:</p>
+            <h4 class="card-title mb-0">$12.99 / Month</h4>
+            <p class="my-0 py-0">or</p>
+            <h5 class="card-title">$150.00 / Year</h5>
+          </li>
+        </ul>
+        <div class="card-footer">
+          <button class="btn btn-primary">Learn More</button>
         </div>
       </div>
     </div>
-  </section>
+
+    <div class="col-12 col-md-4 text-center">
+      <div class="card">
+        <div class="card-header">
+          <h2 class="card-title">Business Hosting</h2>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><strong>Unlimited</strong> Bandwidth</li>
+          <li class="list-group-item"><strong>Unlimited</strong> Disk Space</li>
+          <li class="list-group-item">1 to Unlimited Domains</li>
+          <li class="list-group-item">
+            <p class="my-1">Packages starting at:</p>
+            <h4 class="card-title mb-0">$17.99 / Month</h4>
+            <p class="my-0 py-0">or</p>
+            <h5 class="card-title">$200.00 / Year</h5>
+          </li>
+        </ul>
+        <div class="card-footer">
+          <button class="btn btn-primary">Learn More</button>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12 col-md-4 text-center">
+      <div class="card">
+        <div class="card-header">
+          <h2 class="card-title">VPS Hosting</h2>
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item"><strong>RAM:</strong> 1GB to 96GB</li>
+          <li class="list-group-item"><strong>Processor:</strong> 1 CPU Core to 16 CPU Core</li>
+          <li class="list-group-item"><strong>Storage:</strong> 30GB to 750GB</li>
+          <li class="list-group-item"><strong>Bandwidth:</strong> 1TB to 9TB</li>
+          <li class="list-group-item">
+            <p class="my-1">Packages starting at:</p>
+            <h4 class="card-title mb-0">$8.99 / Month</h4>
+            <p class="my-0 py-0">or</p>
+            <h5 class="card-title">$100.00 / Year</h5>
+          </li>
+        </ul>
+        <div class="card-footer">
+          <button class="btn btn-primary">Learn More</button>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+</section>
+
+<section id="hostingFeatures">
+<div class="container pt-5 pb-4">
+  <div class="row">
+    <div class="col-12 text-center">
+      <h2 class="feature-title">Features Included in All Hosting Plans</h2>
+    </div>
+  </div>
+</div>
+<div class="container pb-5">
+  <div class="row text-center feature-box">
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+  </div>
+</div>
+</section>
