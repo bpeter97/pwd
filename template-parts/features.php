07ff6efb34e45f69c7ba9fb562af68c874a011f7
bpeter97 <brian.lee.peter@gmail.com>
2019-08-24 17:54:34 -0700

    Add features and update icons / services text

---
+++ template-parts/features.php
@@ -0,0 +1,66 @@
+<style>
+
+  .feature {
+    padding-left: 2rem;
+    padding-top: 2rem;
+  }
+
+  .feature.last {
+    padding-bottom: 2rem;
+  }
+  i {
+    font-size: 1.5rem;
+    padding-right: .5rem;
+  }
+
+</style>
+
+<section id="hostingFeatures">
+<div class="container pt-5 pb-4">
+  <div class="row">
+    <div class="col-12 text-center">
+      <h2 class="feature-title">Features Included in All Hosting Plans</h2>
+    </div>
+  </div>
+</div>
+<div class="container pb-5">
+  <div class="row text-left feature-box">
+    <div class="col-sm-4 feature first">
+      <i class="far fa-hdd"></i>
+      <span>Solid State Drives</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="fas fa-cog"></i>
+      <span>Instant Setup</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="fab fa-cpanel"></i>
+      <span>Latest cPanel</span>
+    </div>
+    <div class="col-sm-4 feature first">
+      <i class="fas fa-mail-bulk"></i>
+      <span>Unlimited Email Accounts</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="fas fa-database"></i>
+      <span>Unlimited Databases</span>
+    </div>
+    <div class="col-sm-4 feature">
+      <i class="fas fa-window-restore"></i>
+      <span>Unlimited Subdomains</span>
+    </div>
+    <div class="col-sm-4 feature last">
+      <i class="fas fa-map-marker-alt"></i>
+      <span>Free Dedicated IP</span>
+    </div>
+    <div class="col-sm-4 feature last">
+      <i class="fab fa-weebly"></i>
+      <span>Weebly Site Builder</span>
+    </div>
+    <div class="col-sm-4 feature last">
+      <i class="fas fa-magic"></i>
+      <span>Softaculous Auto Install</span>
+    </div>
+  </div>
+</div>
+</section>
--- template-parts/services.php
+++ template-parts/services.php
@@ -6,10 +6,10 @@
 					<div class="single-service">
 						<i class="fas fa-server"></i>
 						<h4>Web Hosting</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
+						<p>Whether you are looking to host a personal website or a business website, we can provide the soluction you are looking for! Reliable and secure, if you have any issues there is always a support system set up to solve your problems.</p>
 						<button type="button" class="btn btn-primary mt-3">
-										Learn More
-									</button>
+							Learn More
+						</button>
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6">
@@ -17,10 +17,10 @@
 					<div class="single-service">
 						<i class="fa fa-laptop"></i>
 						<h4>Web Design </h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
+						<p>Everything about your website, from content, the way it looks, the way it works, and the way it feels is all a part of the web design. Our process goes over conceptualizing, planning, wireframing, and developing your website! You are included in every step of our design process so your input will always be reviewed.</p>
 						<button type="button" class="btn btn-primary mt-3">
-								Learn More
-							</button>
+							Learn More
+						</button>
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6">
@@ -28,10 +28,10 @@
 					<div class="single-service">
 						<i class="fa fa-mobile"></i>
 						<h4>Responsive Design</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
+						<p>With more and more customers using mobile devices to browse the web and to do business online, it only makes sense to ensure that your website is responsive. This will ensure that you are not limiting the traffic to your website due to device incompatability.</p>
 						<button type="button" class="btn btn-primary mt-3">
-								Learn More
-							</button>
+							Learn More
+						</button>
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6">
@@ -39,10 +39,10 @@
 					<div class="single-service">
 						<i class="fas fa-search"></i>
 						<h4>Search Engine Optimization</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
+						<p>SEO is not just about search engines. Good SEO practices improve the user experience and usability of a web site. Users trust search engines and having a presence in the top rankings automatically builds trust for the user. Therefore it is important to ensure that your website is optimized for search engines but also to ensure that the strategy developed is working over time.</p>
 						<button type="button" class="btn btn-primary mt-3">
-								Learn More
-							</button>
+							Learn More
+						</button>
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6">
@@ -50,10 +50,10 @@
 					<div class="single-service">
 						<i class="fas fa-edit"></i>
 						<h4>Content Management</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
+						<p>You run your business, we will run your website! Do you want a website, but do not want the hastle of making changes on your website yourself? Then this may be an option for you! We will change images, alter information, redesign pages, create new pages, all for $50.00 per hour. Check out what is all included in content management below!</p>
 						<button type="button" class="btn btn-primary mt-3">
-								Learn More
-							</button>
+							Learn More
+						</button>
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6">
@@ -61,7 +61,7 @@
 					<div class="single-service">
 						<i class="fas fa-shopping-cart"></i>
 						<h4>E-Commerce</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
+						<p>Looking to sell products online? We can help you do that. E-commerce has become a successful way for businesses to reach their customers in a much easier manner. Using WooComerce, we can get you set up and running quickly!</p>
 						<button type="button" class="btn btn-primary mt-3">
 								Learn More
 							</button>
@@ -72,10 +72,10 @@
 					<div class="single-service">
 						<i class="fas fa-code"></i>
 						<h4>Custom Development</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
+						<p>Have a more complex project that you want developed? Perhaps a web application that is not necessarily a web site. Perhaps it serves a separate service for the backend part of your business? Contact us so we can talk more about it and see what we can do to help create a solution for you!</p>
 						<button type="button" class="btn btn-primary mt-3">
-									Learn More
-								</button>
+							Learn More
+						</button>
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6">
@@ -83,10 +83,10 @@
 					<div class="single-service">
 						<i class="fas fa-tools"></i>
 						<h4>Web Maintenance</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
+						<p>Owning and managing a website is much more than just throwing some pages together and letting it sit pretty. Checking if all pages are loading with no errors, running backups, updating website assets, updating plugins, checking all forms, removing spam, and checking broken links are some examples of maintenance that should be done weekly. See how we can help maintain your website!</p>
 						<button type="button" class="btn btn-primary mt-3">
-								Learn More
-							</button>
+							Learn More
+						</button>
 					</div>
 				</div>
 				<div class="col-lg-4 col-md-6">
@@ -94,10 +94,10 @@
 					<div class="single-service">
 						<i class="fas fa-exclamation-triangle"></i>
 						<h4>Emergency Services</h4>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry typesetting industry </p>
+						<p>Run into some errors that you cannot fix? Need it fixed ASAP? We provide emergency services and charge at an hourly rate.</p>
 						<button type="button" class="btn btn-primary mt-3">
-								Learn More
-							</button>
+							Learn More
+						</button>
 					</div>
 				</div>
 			</div>
