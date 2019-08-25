93ebb7135f3e4ca0412c098cc1f8dd0b8f213ed9
bpeter97 <brian.lee.peter@gmail.com>
2019-08-24 21:31:27 -0700

    Add background color to contact form

--- template-parts/contact-form.php
+++ template-parts/contact-form.php
@@ -1,16 +1,24 @@
+<style>
+	.wpcf7-form-control.wpcf7-text,
+	.wpcf7-form-control.wpcf7-select,
+	.wpcf7-form-control.wpcf7-textarea {
+		background-color: #fffcf6;
+	}
+</style>
+
 <section id="contact">
-		<div class="container pt-5 pb-5">
-			<div class="row">
-				<div class="col">
-					<p class="text-center">
-						<?= get_theme_mod('contact_form_message', 'Edit the "Contact Form Message" in the customizer.'); ?>
-					</p>
-					<div class="card">
-						<div class="card-body">
-							<?= do_shortcode(get_theme_mod('contact_form_shortcode', 'Edit the "Contact Form Shortcode" in the customizer.')); ?>
-						</div>
+	<div class="container pt-5 pb-5">
+		<div class="row">
+			<div class="col">
+				<p class="text-center">
+					<?= get_theme_mod('contact_form_message', 'Edit the "Contact Form Message" in the customizer.'); ?>
+				</p>
+				<div class="card">
+					<div class="card-body">
+						<?= do_shortcode(get_theme_mod('contact_form_shortcode', 'Edit the "Contact Form Shortcode" in the customizer.')); ?>
 					</div>
 				</div>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
