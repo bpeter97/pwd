cf84995b245200caff0dd7f2a8f7bd4de200bcd3
bpeter97 <brian.lee.peter@gmail.com>
2019-08-24 20:24:21 -0700

    Add SEO Page

---
+++ template-parts/seo.php
@@ -0,0 +1,95 @@
+<style>
+
+  .feature {
+    padding-left: 2rem;
+    padding-top: 2rem;
+  }
+
+  .feature.last {
+    padding-bottom: 2rem;
+  }
+  i {
+    font-size: 1.5rem;
+    padding-right: .5rem;
+  }
+
+</style>
+
+<section id="missionStatement">
+	<div class="container">
+		<div class="d-flex flex-row">
+			<div class="col text-center">
+				<span class="h5">
+						<?= get_theme_mod('seo_statement_message', 'Search Engine Optimization (SEO) is important because the majority of search engine users are more likely to click on the first five results that show in a search, practicing SEO improves users experience on your website, supports social promotion, gains users trust by appearing on search engines, and it puts you ahead of the competition when you are easy to find.'); ?>
+					</span>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="hostingFeatures">
+  <div class="container pt-5 pb-4">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h2 class="feature-title">Search Engine Optimization</h2>
+        <h2 class="feature-title">$100.00 Per Month</h2>
+        <h4 class="feature-title">$1000.00 Per Year (17% discount)</h4>
+      </div>
+    </div>
+  </div>
+  <div class="container pb-5">
+    <div class="row text-left feature-box">
+      <div class="col-sm-4 feature first">
+        <i class="far fa-edit"></i>
+        <span>Keyword Planning</span>
+      </div>
+      <div class="col-sm-4 feature">
+        <i class="fab fa-megaport"></i>
+        <span>Website Structure Review</span>
+      </div>
+      <div class="col-sm-4 feature">
+        <i class="fas fa-mobile-alt"></i>
+        <span>Mobile Testing</span>
+      </div>
+      <div class="col-sm-4 feature first">
+        <i class="fas fa-link"></i>
+        <span>Page URL Review</span>
+      </div>
+      <div class="col-sm-4 feature">
+        <i class="fas fa-tags"></i>
+        <span>Title Tags</span>
+      </div>
+      <div class="col-sm-4 feature">
+        <i class="far fa-images"></i>
+        <span>Image Information</span>
+      </div>
+      <div class="col-sm-4 feature">
+        <i class="fas fa-paragraph"></i>
+        <span>Text Review</span>
+      </div>
+      <div class="col-sm-4 feature">
+        <i class="fas fa-file-alt"></i>
+        <span>Content Review</span>
+      </div>
+      <div class="col-sm-4 feature">
+        <i class="fas fa-rss-square"></i>
+        <span>Meta Descriptions</span>
+      </div>
+      <div class="col-sm-4 feature last">
+        <i class="fas fa-link"></i>
+        <span>Internal Linking</span>
+      </div>
+      <div class="col-sm-4 feature last">
+        <i class="fas fa-share-square"></i>
+        <span>Sitemap Submission</span>
+      </div>
+      <div class="col-sm-4 feature last">
+        <i class="fas fa-chart-line"></i>
+        <span>SEO Plan Development</span>
+      </div>
+      <div class="col-sm-12 text-center">
+        <button class="btn btn-primary">Contact Us</button>
+      </div>
+    </div>
+  </div>
+</section>
