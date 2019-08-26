481208cb8da74a3cd86134662ce47047f4c3e087
bpeter97 <brian.lee.peter@gmail.com>
2019-08-26 15:24:47 -0700

    Change technologies to font awesome icons instead of images

--- template-parts/tech-section.php
+++ template-parts/tech-section.php
@@ -1,15 +1,22 @@
+<style>
+	#technologies i {
+		color: white;
+		font-size: 5rem;
+	}
+</style>
+
 <section id="technologies" class="d-none d-xl-block">
-		<div class="container-fluid">
-			<div class="d-flex flex-row justify-content-around">
-				<img src="<?= get_bloginfo('template_url').'/assets/imgs/technologies/wordpress-logo.png'; ?>" alt="Wordpress" />
-				<img src="<?= get_bloginfo('template_url').'/assets/imgs/technologies/nodejs-icon.png'; ?>" alt="Node.JS" />
-				<img src="<?= get_bloginfo('template_url').'/assets/imgs/technologies/php.png'; ?>" alt="PHP" />
-				<img src="<?= get_bloginfo('template_url').'/assets/imgs/technologies/mysql.png'; ?>" alt="My SQL" />
-				<img src="<?= get_bloginfo('template_url').'/assets/imgs/technologies/mongodb.png'; ?>" alt="MongoDB" />
-				<img src="<?= get_bloginfo('template_url').'/assets/imgs/technologies/bstrap.png'; ?>" alt="Bootstrap" />
-				<img src="<?= get_bloginfo('template_url').'/assets/imgs/technologies/html.png'; ?>" alt="HTML" />
-				<img src="<?= get_bloginfo('template_url').'/assets/imgs/technologies/css.png'; ?>" alt="CSS" />
-				<img src="<?= get_bloginfo('template_url').'/assets/imgs/technologies/seo.png'; ?>" alt="Search Engine Optimization" />
-			</div>
+	<div class="container-fluid">
+		<div class="d-flex flex-row justify-content-around">
+			<i class="fab fa-wordpress-simple"></i>
+			<i class="fab fa-node-js"></i>
+			<i class="fab fa-php"></i>
+			<i class="fab fa-bootstrap"></i>
+			<i class="fab fa-html5"></i>
+			<i class="fab fa-css3-alt"></i>
+			<i class="fab fa-searchengin"></i>
+			<i class="fab fa-angular"></i>
+			<i class="fab fa-js-square"></i>
 		</div>
-	</section>
+	</div>
+</section>
